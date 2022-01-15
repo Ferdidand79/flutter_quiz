afb591570a76c80b3829626a20d81918a1c2d14e
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-01-15 16:22:38 -0500

    subeindo proyecto

---
+++ android/app/src/main/kotlin/com/ferdidand79/flutter_quiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ferdidand79.flutter_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //STEP 1: Nesecitamos crear la estructura de folderes del proyecto
+  //STEP 2: Necesitamos crear el modelo de cuestionario QUIZZ
+  //STEP 3: Nesecitamos una lista simple de preguntas display it
+  //en nuestra pantalla, por supuesto, puede usar su propia lista u obtener datos
+  //desde un API existente
+  //empecemos
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
